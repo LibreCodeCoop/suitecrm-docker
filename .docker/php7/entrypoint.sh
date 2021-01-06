dcfc1fac4967bb6f0d3526813681c0bc8761887e
Vitor Mattos <vitor@php.rio>
2021-01-06 16:23:37 -0300

    Update entrypoint.sh

--- .docker/php7/entrypoint.sh
+++ .docker/php7/entrypoint.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
-. `pwd`/.env
+#!/bin/sh
 if [ ! -d "SuiteCRM" ]; then
     export COMPOSER_ALLOW_SUPERUSER=1
     composer global require hirak/prestissimo
 
-    git clone https://github.com/salesagility/SuiteCRM
+    git clone --progress -b "${SUITECRM_VERSION}" --single-branch --depth 1 https://github.com/salesagility/SuiteCRM /tmp/suitecrm
+    rsync -r /tmp/suitecrm/ SuiteCRM
+    rm -rf /tmp/suitecrm
     cd SuiteCRM
     composer install
     chown -R www-data:www-data .
     chmod -R 755 .
     chmod -R 775 custom modules themes data upload
     # cache
-    # chmod 775 config_override.php 2>/dev/null
+    chmod 775 config_override.php 2>/dev/null
 fi
-cron
 php-fpm
